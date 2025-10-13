c673a33df6180dddca4457ab53c6e5b0fffee27a
Steve Adams <stevezau@gmail.com>
2025-10-13 17:22:18 +1100

    fix version check

--- plex_generate_previews/__init__.py
+++ plex_generate_previews/__init__.py
@@ -5,11 +5,7 @@ A tool for generating video preview thumbnails for Plex Media Server.
 Supports GPU acceleration (NVIDIA, AMD, Intel, WSL2) and CPU processing.
 """
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    # Fallback for development without installation
-    __version__ = "0.0.0.dev0"
+from ._version import __version__
 
 __author__ = "stevezau"
 __description__ = "Generate video preview thumbnails for Plex Media Server"
